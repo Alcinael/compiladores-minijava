9aa07f3a229f888753f260411cfe33d429b5fc57
airton.ufc <airton.ufc@gmail.com>
2012-05-01 22:15:27 +0000

    TypeCheckingVisitor

--- Compiladores/src/syntaxtree/IntegerType.java
+++ Compiladores/src/syntaxtree/IntegerType.java
@@ -3,6 +3,18 @@ import visitor.Visitor;
 import visitor.TypeVisitor;
 
 public class IntegerType extends Type {
+  public int value;
+	
+  public IntegerType() {
+	super();
+	// TODO Auto-generated constructor stub
+  }
+  
+  public IntegerType(int value) {
+	super();
+	this.value = value;
+  }
+
   public void accept(Visitor v) {
     v.visit(this);
   }
@@ -10,4 +22,5 @@ public class IntegerType extends Type {
   public Type accept(TypeVisitor v) {
     return v.visit(this);
   }
+
 }
