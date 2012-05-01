44f9a7f24fe8ed814c322dfe95ff07c4cf81c8db
airton.ufc <airton.ufc@gmail.com>
2012-05-01 22:15:57 +0000

    TypeCheckingVisitor

--- Compiladores/src/syntaxtree/This.java
+++ Compiladores/src/syntaxtree/This.java
@@ -3,6 +3,17 @@ import visitor.Visitor;
 import visitor.TypeVisitor;
 
 public class This extends Exp {
+  public Identifier i;
+  	
+  public This() {
+	super();
+  }
+  
+  public This(Identifier i) {
+	super();
+	this.i = i;
+  }
+
   public void accept(Visitor v) {
     v.visit(this);
   }
