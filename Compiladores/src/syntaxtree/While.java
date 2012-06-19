5144fe12c182852da56e4b03a3396c51d7e31e70
airton.ufc <airton.ufc@gmail.com>
2012-06-19 13:33:53 +0000

    Type Checking

--- Compiladores/src/syntaxtree/While.java
+++ Compiladores/src/syntaxtree/While.java
@@ -1,4 +1,5 @@
 package syntaxtree;
+import symbol.Table;
 import visitor.Visitor;
 import visitor.TypeVisitor;
 
@@ -17,4 +18,19 @@ public class While extends Statement {
   public Type accept(TypeVisitor v) {
     return v.visit(this);
   }
+
+  @Override
+  public Table identifiers(Table t) {
+	this.e.identifiers(t);
+	this.s.identifiers(t);
+	return t;
+  }
+
+  @Override
+  public Table removeIdentifiers(Table t) {
+	this.e.removeIdentifiers(t);
+	this.s.removeIdentifiers(t);
+	return t;
+  }
+
 }
